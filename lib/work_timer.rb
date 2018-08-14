dad1683be4b805bac86e2b786f904b4333c7333d
9sako6 <ty@TAIRAnoMacBook-Air.local>
2018-08-15 01:40:22 +0900

    version 0.1.0

--- lib/work_timer.rb
+++ lib/work_timer.rb
@@ -5,4 +5,74 @@ module WorkTimer
   def self.greet
     'Hello'
   end
+  
+  class Work
+    def initialize(fileName)
+      @fileName = fileName
+    end
+
+    def start_work(startTime=nil)
+      @startTime ||= Time.now
+    end
+
+    def end_work(endTime=nil)
+      @endTime ||= Time.now
+      stop_view
+    end
+
+    # 戻り値はString
+    def total_time
+      totalTime = 0
+      begin
+        f = File.open(@fileName, "r")
+      rescue
+        return sec2seq(totalTime)
+      end
+      f.each_line do |line|
+        next if line =~ /^\d{4}-\d{2}-\d{2}/
+        # **:**:**表示を秒に変換
+        line.chomp.split(":").each_with_index do |time, i|
+          totalTime += time.to_i*60**(2-i)
+        end
+      end
+      f.close
+      sec2seq(totalTime)
+    end
+
+    def record_time
+      File.open(@fileName, "a+") do |f|
+        # 標準で09:00:00となるのを00:00:00にするために60*60*15足してる
+        workTime = (@endTime-@startTime.to_i+60*60*15).strftime("%T")
+        f.puts @startTime, @endTime, workTime
+      end
+    end
+
+    def view
+      @view_thread = Thread.new do
+        loop do
+          workingTime = Time.now - @startTime.to_i
+          print "\r\033[32m#{(workingTime+60*60*15).strftime("%T")}\033[30m"
+          sleep 1
+        end
+      end
+    end
+
+    private
+
+    def sec2seq(sec)
+      hours = (sec/3600.0).to_i.to_s
+      minutes = ((sec-(sec/3600.0).to_i*3600.0)/60).to_i.to_s
+      seconds = (sec%60).to_i.to_s
+      "#{two_digits(hours)}:#{two_digits(minutes)}:#{two_digits(seconds)}"
+    end
+
+    # 1桁の数字（実際はStringクラス）を2桁にする (e.g.: 0:0:47 -> 00:00:47)
+    def two_digits(time)
+      time.size==1 ? "0"+time : time
+    end
+
+    def stop_view
+      @view_thread.kill if @view_thread && @view_thread.alive?
+    end
+  end
 end
